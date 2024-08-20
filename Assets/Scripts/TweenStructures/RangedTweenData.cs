f453804de8b30cf6c671584577ebc487aa43487e
Julia <hunter-777@yandex.ru>
2024-08-20 18:36:09 +0300

    Created range tween datas

---
+++ Assets/Scripts/TweenStructures/RangedTweenData.cs
@@ -0,0 +1,13 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts.TweenStructures
+{
+    public class Vector3RangedTweenData : RangedTweenData<Vector3> { }
+	public class Vector2RangedTweenData : RangedTweenData<Vecror2> { }
+	publiccla RanhedTweenData<T> : TweenData <T>
+
+	{
+		pumlic T From;
+	}
+}

afc01707a2ba4cfb74740bcdd701d9a024675ff7
Julia <hunter-777@yandex.ru>
2024-08-22 15:28:59 +0300

    ПОИСК ОШИБКИ

---
+++ Assets/Scripts/Animation/SettingsButtonDropdownAnimation.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts.TweenStructures;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Animation
+{
+    public class SettingsButtonDropdownAnimation : MonoBehaviour
+    {
+        [SerializeField] private Vector2RangedTweenData buttonsTweenData;
+        [SerializeField] private GameObject actionRoot;
+        [SerializeField] private float delayBetweenButtons;
+
+        private RectTransform[] _buttonTransforms;
+        private bool _active;
+        private RectTransform _buttonTransform;
+
+        private void Start()
+        {
+            _buttonTransform = actionRoot.GetComponentInChildren<RectTransform>();
+            _buttonTransforms = new RectTransform[] { _buttonTransform };
+            TweenButtonSize(_active, buttonsTweenData);
+        }
+
+        public void OnButtonClicked()
+        {
+            _active = !_active;
+            TweenButtonSize(_active, buttonsTweenData);
+        }
+        private void TweenButtonSize(bool active, Vector2RangedTweenData tweenData)
+        {
+            float delay = 0.0f;
+
+            foreach (RectTransform buttonTransform in _buttonTransforms)
+            {
+                Vector2 sizeDelta = active ? tweenData.To : tweenData.From;
+                buttonTransform
+                    .DOSizeDelta(sizeDelta,tweenData.Duration)
+                    .SetDelay(delay)
+                    .SetEase(tweenData.Ease);
+                delay += delayBetweenButtons;
+            }
+        }
+
+    }
+}
--- Assets/Scripts/TweenStructures/RangedTweenData.cs
+++ Assets/Scripts/TweenStructures/RangedTweenData.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 namespace Assets.Scripts.TweenStructures
 {
+    [System.Serializable]
     public class Vector3RangedTweenData : RanhedTweenData<Vector3> { }
+	
+	[System.Serializable]
 	public class Vector2RangedTweenData : RanhedTweenData<Vector2> { }
 	public class RanhedTweenData<T> : TweenData <T>
 
 	{
 		public T From;
+		public T To;
 	}
 }
